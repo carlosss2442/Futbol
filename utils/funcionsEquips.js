33e84be96d77fe8b8822058cc32b6f020bed9ae3
Carlos Teran Sanchez <catesa@alumnatflorida.es>
2026-05-25 21:51:29 +0200

    ejercicio 2 funcionsEquips

--- utils/funcionsEquips.js
+++ utils/funcionsEquips.js
@@ -12,7 +12,7 @@ export const getObjEquip = (nomEquip) => {
 //a partir d'un obj amb les dades d'un equip, obtinc la imatge de l'escut (strBadge)
 const getUrlImatgeEscut = (unEquipObj) => {
     //implementeu-ho
-    return unEquipObj.strBadge;
+    return (unEquipObj.strBadge);
 }
 //const equip = getObjEquip("Barcelona");
 //console.log(getUrlImatgeEscut(equip[0]));
@@ -20,7 +20,7 @@ const getUrlImatgeEscut = (unEquipObj) => {
 //obtinc l'any de fundació (intFormedYear)
 const getAnyFundacio = (unEquipObj) => {
     //implementeu-ho
-    return unEquipObj.intFormedYear;
+    return (unEquipObj.intFormedYear);
 }
 
 /* const equip2 = getObjEquip("Valencia");
@@ -29,7 +29,7 @@ console.log(getAnyFundacio(equip2[0])); */
 //obtinc El nom de l'estadi (strStadium)
 const getNomEstadi = (unEquipObj) => {
     //implementeu-ho
-    return unEquipObj.strStadium;
+    return (unEquipObj.strStadium);
 }
 /* const equip3 = getObjEquip("Valencia");
 console.log(getNomEstadi(equip3[0])); */
@@ -37,21 +37,26 @@ console.log(getNomEstadi(equip3[0])); */
 //obtinc el nom de l'equip (strTeam)
 const getNomEquip = (unEquipObj) => {
     //implementeu-ho
-    return unEquipObj.strTeam;
+    return (unEquipObj.strTeam);
 }
 /* const equipo = getObjEquip("Barcelona")
 console.log(getNomEquip(equipo[0])); */
+
+const getLigaEquipo = (unEquipObj) => {
+    return ([unEquipObj.strLeague, unEquipObj.strLeague2, unEquipObj.strLeague3, unEquipObj.strLeague4])
+}
 
 //obtinc la fitxa de l'equip.
 //La fitxa consisteix en un objecte amb els atributs: nom,imatgeEscut,anyFundacio,estadi
 export const getFitxaEquip = (unEquipObj) => {
     //implementeu-ho
     return ({
-        nombre: unEquipObj.strTeam,
-        escudo: unEquipObj.strBadge,
-        anyoFun: unEquipObj.intFormedYear,
-        estadio: unEquipObj.strStadium,
-        capacidad: unEquipObj.intStadiumCapacity
+        nombre: getNomEquip(unEquipObj),
+        escudo: getUrlImatgeEscut(unEquipObj),
+        anyoFun: getAnyFundacio(unEquipObj),
+        estadio: getNomEstadi(unEquipObj),
+        capacidad: getLigaEquipo(unEquipObj),
+
     })
 }
 
