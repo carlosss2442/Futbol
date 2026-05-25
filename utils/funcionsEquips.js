563c351ed718540139e33aab8628532741ac4c6d
Carlos Teran Sanchez <catesa@alumnatflorida.es>
2026-05-25 17:32:27 +0200

    utils creado

--- utils/funcionsEquips.js
+++ utils/funcionsEquips.js
@@ -1,39 +1,60 @@
 
-import * as file from '../assets/dades/dades.json' with { type: "json" };
+import * as file from '../assets/dades/dades2.json' with { type: "json" };
 const dades = file.default.equips;//dades és un array d'equips, ara
 
 // A partir d'un nom d'un equip ('Valencia', 'Levante', 'Barcelona', ... ), 
 // obtinc l'objecte d'eixe equip amb totes seues dades.
-const getObjEquip = (nomEquip)=> {
+const getObjEquip = (nomEquip) => {
     //imprementeu-ho
+    return (dades.filter(ele => ele.strTeam == nomEquip));
 }
-
+//console.log(getObjEquip("Valencia"));
 //a partir d'un obj amb les dades d'un equip, obtinc la imatge de l'escut (strBadge)
-const getUrlImatgeEscut = (unEquipObj)=>{
+const getUrlImatgeEscut = (unEquipObj) => {
     //implementeu-ho
+    return unEquipObj.strBadge;
 }
+//const equip = getObjEquip("Barcelona");
+//console.log(getUrlImatgeEscut(equip[0]));
 
 //obtinc l'any de fundació (intFormedYear)
-const getAnyFundacio = (unEquipObj)=>{
+const getAnyFundacio = (unEquipObj) => {
     //implementeu-ho
+    return unEquipObj.intFormedYear;
 }
+
+/* const equip2 = getObjEquip("Valencia");
+console.log(getAnyFundacio(equip2[0])); */
 
 //obtinc El nom de l'estadi (strStadium)
-const getNomEstadi = (unEquipObj)=>{
+const getNomEstadi = (unEquipObj) => {
     //implementeu-ho
+    return unEquipObj.strStadium;
 }
+/* const equip3 = getObjEquip("Valencia");
+console.log(getNomEstadi(equip3[0])); */
 
 //obtinc el nom de l'equip (strTeam)
-const getNomEquip = (unEquipObj)=>{
-   //implementeu-ho
+const getNomEquip = (unEquipObj) => {
+    //implementeu-ho
+    return unEquipObj.strTeam;
 }
+/* const equipo = getObjEquip("Barcelona")
+console.log(getNomEquip(equipo[0])); */
 
 //obtinc la fitxa de l'equip.
 //La fitxa consisteix en un objecte amb els atributs: nom,imatgeEscut,anyFundacio,estadi
-const getFitxaEquip = (unEquipObj)=>{
-   //implementeu-ho
+const getFitxaEquip = (unEquipObj) => {
+    //implementeu-ho
+    return ({
+        nombre: unEquipObj.strTeam,
+        escudo: unEquipObj.strBadge,
+        anyoFun: unEquipObj.intFormedYear,
+        estadio: unEquipObj.strStadium,
+    })
 }
 
-const equipACercar = 'Girona';
-
+const equipACercar = 'Barcelona';
+const objectEquip = getObjEquip(equipACercar)
+console.log(getFitxaEquip(objectEquip[0]))
 
